13873e6d39ee6ce64b963ad9e0aaf558a6afac38
muhammadmusammil <muhammadmusammil610@gmail.com>
2025-03-26 11:32:02 +0530

    constant added

---
+++ constant/__init__.py
@@ -0,0 +1,13 @@
+
+
+KNOWN_HEIGHT: float = 1.7
+REFERENCE_DISTANCE: float = 0.9
+
+FRAME_WIDTH: int = 640
+FRAME_HEIGHT: int = 480
+
+FRAME_RESIZE: int = 320
+
+CONF: float = 0.25
+
+MODEL_PATH = r"runs\detect\train3\weights\best.pt"
