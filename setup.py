8559f34e0859b49ae51217244efa2f496bafb137
muhammadmusammil <muhammadmusammil610@gmail.com>
2025-03-26 11:31:17 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="object_detection_using_YOLO",
+    version="0.0.0",
+    author="muhammadmusammil",
+    author_email="muhammadmusammil610@gmail.com",
+    packages=find_packages()
+    )
